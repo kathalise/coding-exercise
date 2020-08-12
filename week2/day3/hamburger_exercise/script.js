c24e2bbb99ba703f629c6223be8e0f80e4e93f3d
Katharina Wolf <wolfkatharina89@gmail.com>
2020-08-12 22:43:00 +0200

    Hamburger Exercise

---
+++ week2/day3/hamburger_exercise/script.js
@@ -0,0 +1,20 @@
+console.log("sanity check!");
+
+(function () {
+    var sideNav = document.getElementsByClassName("side-nav")[0];
+    var overlay = document.getElementsByClassName("overlay")[0];
+    var hamburger = document.getElementById("hamburger");
+    var close = document.getElementById("x");
+
+    hamburger.addEventListener("click", function () {
+        sideNav.classList.remove("on");
+        sideNav.style.visibility = "visible";
+        overlay.style.visibility = "visible";
+    });
+
+    close.addEventListener("click", function () {
+        sideNav.classList.add("on");
+        sideNav.style.visibility = "hidden";
+        overlay.style.visibility = "hidden";
+    });
+})();
