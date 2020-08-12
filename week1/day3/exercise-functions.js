61319c587bd4a0f12593569c96a1454f8bf4f33f
Katharina Wolf <wolfkatharina89@gmail.com>
2020-08-12 23:10:46 +0200

    Day3 Exercise

---
+++ week1/day3/exercise-functions.js
@@ -0,0 +1,38 @@
+//Exercise 1:
+function sum() {
+    var sum = 0;
+    for (var x = 0; x < arguments.length; x++) {
+        sum += arguments[x];
+    }
+    return sum;
+}
+
+// console.log(sum(5, 10));
+// console.log(sum(5, 10, 15));
+// console.log(sum(5, 10, 15, 100, 200));
+
+//Exercise 2:
+function waitThenRun(arg) {
+    setTimeout(arg, 1500);
+}
+
+waitThenRun(function () {
+    console.log("Hello!");
+    waitThenRun(function () {
+        console.log("Goodbye!");
+    });
+});
+
+//Exercise 3:
+function myFunction(num) {
+    if (num <= 0 || isNaN(num)) {
+        return "ERROR";
+    } else if (num >= 1000000) {
+        return num;
+    } else {
+        return (num = myFunction(num * 10));
+    }
+}
+
+// console.log(myFunction(0));
+// console.log(myFunction(5));
